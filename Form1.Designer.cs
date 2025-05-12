5ee36391173303b1a8f87a69d7debf96bddad5f2
Biohive <clemens.genius@gmail.com>
2025-05-12 08:28:23 -0500

    Initial files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.minButton1 = new no_idle.controls.minButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,6 +43,23 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // minButton1
+            // 
+            this.minButton1.BackColor = System.Drawing.Color.White;
+            this.minButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.minButton1.FlatAppearance.BorderSize = 0;
+            this.minButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.minButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.minButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.minButton1.Font = new System.Drawing.Font("Webdings", 12F);
+            this.minButton1.ForeColor = System.Drawing.Color.White;
+            this.minButton1.Location = new System.Drawing.Point(128, 51);
+            this.minButton1.Name = "minButton1";
+            this.minButton1.Size = new System.Drawing.Size(64, 25);
+            this.minButton1.TabIndex = 1;
+            this.minButton1.Text = "minButton1";
+            this.minButton1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -50,6 +68,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(231, 362);
             this.ControlBox = false;
+            this.Controls.Add(this.minButton1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
@@ -68,6 +87,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private controls.minButton minButton1;
     }
 }
 
---
+++ controls/minButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace no_idle.controls
+{
+    public class minButton : Button
+    {
+        public minButton()
+        {
+            this.Size = new Size(30, 30);
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 0, 0, 0);
+            this.FlatAppearance.MouseDownBackColor = Color.FromArgb(255, 0, 0, 0);
+            this.BackColor = Color.FromArgb(255, 0, 0, 0);
+            this.ForeColor = Color.White;
+            this.Font = new Font("Webdings", 12);
+            this.Text = "0";
+        }
+    }
+}
